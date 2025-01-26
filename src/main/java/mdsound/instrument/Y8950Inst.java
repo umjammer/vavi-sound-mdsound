062602c090fab5e4d4f1f3e58f6e18eabafdf947
Naohide Sano <namachapanda2005@gmail.com>
2025-01-26 21:45:46 +0900

    [y8950] make it work

--- src/main/java/mdsound/instrument/Y8950Inst.java
+++ src/main/java/mdsound/instrument/Y8950Inst.java
@@ -11,6 +11,8 @@ import mdsound.chips.Y8950;
 
 public class Y8950Inst extends Instrument.BaseInstrument implements PcmEnabledInstrument {
 
+    protected static final int CHIP_SAMPLING_MODE = 0;
+
     public static final int DefaultClockValue = 3579545;
     public static final int MAX_CHIPS = 0x02;
 
